e99f4f298da5cdfb47ecf8de57190db69d92e30e
Pavel Voitihovich <pasha2003228@gmail.com>
2022-05-06 18:17:12 +0300

    feat: customize the project structure

---
+++ proman/src/components/Exemple.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react'; //Удалить!!!
+
+const Exemple2: FC = () => {
+  return <h1>Exemple</h1>
+};
+
+export default Exemple2; 
---
+++ proman/src/pages/Welcome.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react'; //Удалить!!!
+
+const Exemple: FC = () => {
+  return <h1>Exemple</h1>
+};
+
+export default Exemple; 
